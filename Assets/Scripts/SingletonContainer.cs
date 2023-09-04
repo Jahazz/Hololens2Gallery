84082f7e91ac0baa7939e95ae59017059802417c
jakub dyja <jakub.dyja@gmail.com>
2023-09-05 01:47:24 +0200

    Added more of codebase

---
+++ Assets/Scripts/SingletonContainer.cs
@@ -0,0 +1,15 @@
+using Codebase;
+using Gallery.FlickrAPIIntegration.Mediator;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gallery.Singletons
+{
+    public class SingletonContainer : SingletonMonobehaviour<SingletonContainer>
+    {
+        [field: SerializeField]
+        public NetworkingMediator NetworkingMediatorInstance { get; private set; }
+    }
+}
+
